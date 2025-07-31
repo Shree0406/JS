1fc9e7b7a9f7931594e16b5e368d31aa18b7b017
Shree0406 <22u1621@students.git.edu>
2025-07-31 09:38:06 +0000

    committed

---
+++ basics/dataTypes.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log(3+3); 
+console.log("java");
+
+let name = "spd"
+let age = 18
+let isLoggedin = true
---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "shree@google.com"
+ accountCity = "jaipur"
+ let accountState;
+
+ accountEmail = "hdfc@google.com"
+ accountCity = "mumbai"
+ console.table([ accountId, accountCity, accountEmail, accountState])
