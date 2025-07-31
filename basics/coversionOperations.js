324c03769f15c28c18d934ee57b241d1d137fa09
Shree0406 <22u1621@students.git.edu>
2025-07-31 10:18:49 +0000

    conversions

---
+++ basics/coversionOperations.js
@@ -0,0 +1,19 @@
+//let score = true 
+
+
+//console.log(typeof score);
+
+//let valueInNumber = Number(score)
+//console.log(valueInNumber);
+
+//let isLoggedin = 1
+//let booleanIsLoggedin = Boolean(isLoggedin)
+//console.log(booleanIsLoggedin);
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof someNumber);
+
